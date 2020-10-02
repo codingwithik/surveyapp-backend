eeddfb95edd3e27b0915b69e9fffa9fe57612159
muilat <muilat.champ@gmail.com>
2020-10-02 08:31:04 +0100

    setting up

--- src/main/resources/db/migration/migration-1.sql
+++ src/main/resources/db/migration/migration-1.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (email, name, phone_number, uuid, password, enable,  role,  address, created_at, updated_at)
+INSERT INTO admin_user (email, first_name, last_name, password, enable, last_login, role, display_name, address,  photo, created_at, updated_at)
     VALUES 
-    ('ik.francis@yahoo.com', 'Ikenna Umeanozie', '07068174707', '000-111-222-333', '$2y$12$nmlpS1jV10FPZtNloOT3VulED34S40KhyCj0ro2.DxxYnbcQYiBtO', 1, 'ROLE_COORDINATOR', 'Lagos',  null, null);
+    ('admin@kadick.ng', 'Ikenna', 'Umeanozie', '$2y$12$nmlpS1jV10FPZtNloOT3VulED34S40KhyCj0ro2.DxxYnbcQYiBtO', 1, null, 'ROLE_SUPERADMIN', 'Ikenna', null, null, null, null);
       
